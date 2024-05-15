59b2add8c6895122dbce4f172fc92ef0e5ce0053
PierreKROB <krobpierre0@gmail.com>
2024-05-15 17:28:06 +0200

    fix deprecated function

--- app/src/main/java/com/pkrob/ApiDocLoL/ui/ItemList.kt
+++ app/src/main/java/com/pkrob/ApiDocLoL/ui/ItemList.kt
@@ -3,7 +3,6 @@ package com.pkrob.ApiDocLoL.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
 import com.pkrob.ApiDocLoL.model.Item
 
@@ -43,7 +43,7 @@ fun ItemRow(item: Item, version: String) {
     Row(modifier = Modifier.padding(8.dp)) {
         val imageUrl = "https://ddragon.leagueoflegends.com/cdn/$version/img/item/${item.image.full}"
         Image(
-            painter = rememberImagePainter(imageUrl),
+            painter = rememberAsyncImagePainter(imageUrl),
             contentDescription = "Item Image",
             modifier = Modifier.size(64.dp),
             contentScale = ContentScale.Crop
