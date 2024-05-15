396316c862b4263d2d230d4f55770ed47a5dd866
PierreKROB <krobpierre0@gmail.com>
2024-05-15 17:59:54 +0200

    add modal + stat start

--- app/src/main/java/com/pkrob/ApiDocLoL/model/Item.kt
+++ app/src/main/java/com/pkrob/ApiDocLoL/model/Item.kt
@@ -13,10 +13,48 @@ data class Item(
     @SerializedName("description")
     val description: String,
     @SerializedName("image")
-    val image: ItemImage
+    val image: ItemImage,
+    @SerializedName("gold")
+    val gold: ItemGold,
+    @SerializedName("tags")
+    val tags: List<String>,
+    @SerializedName("stats")
+    val stats: ItemStats
 )
 
 data class ItemImage(
     @SerializedName("full")
     val full: String
+)
+
+data class ItemGold(
+    @SerializedName("base")
+    val base: Int,
+    @SerializedName("total")
+    val total: Int,
+    @SerializedName("sell")
+    val sell: Int,
+    @SerializedName("purchasable")
+    val purchasable: Boolean
+)
+
+data class ItemStats(
+    @SerializedName("FlatCritChanceMod")
+    val flatCritChanceMod: Double?,
+    @SerializedName("FlatPhysicalDamageMod")
+    val flatPhysicalDamageMod: Double?,
+    @SerializedName("FlatHPPoolMod")
+    val flatHPPoolMod: Double?,
+    @SerializedName("FlatMagicDamageMod")
+    val flatMagicDamageMod: Double?,
+    @SerializedName("PercentAttackSpeedMod")
+    val percentAttackSpeedMod: Double?,
+    @SerializedName("FlatArmorMod")
+    val flatArmorMod: Double?,
+    @SerializedName("FlatSpellBlockMod")
+    val flatSpellBlockMod: Double?,
+    @SerializedName("PercentLifeStealMod")
+    val percentLifeStealMod: Double?,
+    @SerializedName("FlatMovementSpeedMod")
+    val flatMovementSpeedMod: Double?
 )
--- app/src/main/java/com/pkrob/ApiDocLoL/ui/ItemList.kt
+++ app/src/main/java/com/pkrob/ApiDocLoL/ui/ItemList.kt
@@ -1,14 +1,17 @@
 package com.pkrob.ApiDocLoL.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import com.pkrob.ApiDocLoL.model.Item
 
 @Composable
@@ -40,7 +42,13 @@ fun ItemList(items: List<Item>, version: String) {
 
 @Composable
 fun ItemRow(item: Item, version: String) {
-    Row(modifier = Modifier.padding(8.dp)) {
+    val (showDialog, setShowDialog) = remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable { setShowDialog(true) }
+    ) {
         val imageUrl = "https://ddragon.leagueoflegends.com/cdn/$version/img/item/${item.image.full}"
         Image(
             painter = rememberAsyncImagePainter(imageUrl),
@@ -49,8 +57,67 @@ fun ItemRow(item: Item, version: String) {
             contentScale = ContentScale.Crop
         )
         Column(modifier = Modifier.padding(start = 8.dp)) {
-            Text(text = item.name, style = MaterialTheme.typography.headlineMedium)
-            Text(text = item.description, style = MaterialTheme.typography.bodyMedium)
+            Text(text = item.name, style = MaterialTheme.typography.displayMedium)
         }
     }
+
+    if (showDialog) {
+        ItemDetailsDialog(item = item, version = version, onDismiss = { setShowDialog(false) })
+    }
+}
+
+@Composable
+fun ItemDetailsDialog(item: Item, version: String, onDismiss: () -> Unit) {
+    val imageUrl = "https://ddragon.leagueoflegends.com/cdn/$version/img/item/${item.image.full}"
+
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text(text = item.name) },
+        text = {
+            Column {
+                Image(
+                    painter = rememberAsyncImagePainter(imageUrl),
+                    contentDescription = "Item Image",
+                    modifier = Modifier.size(64.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Text(text = item.description)
+                Text(text = "Coût : ${item.gold.total} PO", style = MaterialTheme.typography.bodyMedium)
+                Text(text = "Statistiques :", style = MaterialTheme.typography.bodyMedium)
+                item.stats.flatCritChanceMod?.let {
+                    Text(text = "Chance Critique : ${it * 100}%", style = MaterialTheme.typography.bodyMedium)
+                }
+                item.stats.flatPhysicalDamageMod?.let {
+                    Text(text = "Dégâts Physiques : $it", style = MaterialTheme.typography.bodyMedium)
+                }
+                item.stats.flatHPPoolMod?.let {
+                    Text(text = "Santé : $it", style = MaterialTheme.typography.bodyMedium)
+                }
+                item.stats.flatMagicDamageMod?.let {
+                    Text(text = "Dégâts Magiques : $it", style = MaterialTheme.typography.bodyMedium)
+                }
+                item.stats.percentAttackSpeedMod?.let {
+                    Text(text = "Vitesse d'Attaque : ${it * 100}%", style = MaterialTheme.typography.bodyMedium)
+                }
+                item.stats.flatArmorMod?.let {
+                    Text(text = "Armure : $it", style = MaterialTheme.typography.bodyMedium)
+                }
+                item.stats.flatSpellBlockMod?.let {
+                    Text(text = "Résistance Magique : $it", style = MaterialTheme.typography.bodyMedium)
+                }
+                item.stats.percentLifeStealMod?.let {
+                    Text(text = "Vol de Vie : ${it * 100}%", style = MaterialTheme.typography.bodyMedium)
+                }
+                item.stats.flatMovementSpeedMod?.let {
+                    Text(text = "Vitesse de Déplacement : $it", style = MaterialTheme.typography.bodyMedium)
+                }
+                Text(text = "Tags : ${item.tags.joinToString(", ")}", style = MaterialTheme.typography.bodyMedium)
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Fermer")
+            }
+        }
+    )
 }
